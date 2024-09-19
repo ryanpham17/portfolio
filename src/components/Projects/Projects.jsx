1c77deb52d56eef766ce7b052c8da794c278bb15
Ryan Pham <rpham817@gmail.com>
2024-09-19 03:02:15 -0700

    made cards to display projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React from 'react';
 import styles from './Projects.module.css';
+import './cards.css';
 import { getImageURL } from '../../utils';
+import config from './config.json';
 
 export const Projects = () => {
-  return (
-    <section id = "projects">
-        <div className={styles.spacer}>
-            <p className={styles.title1}>Check Out My Recent</p>
-            <h1 className= {styles.title2}>Projects</h1>
-        </div>
-        <div className={styles.projectsOuterContainer}>
-          <div className={styles.projectsContainer}>
-            <p>Currently Working on Projects</p>
-          </div>
-        </div>
-    </section>
-  )
-}
+  let cardArr = config;
 
+  return (
+    <section id="projects">
+      <div className={styles.spacer}>
+        <p className={styles.title1}>Check Out My Recent</p>
+        <h1 className={styles.title2}>Projects</h1>
+      </div>
+      <div className={styles.cardContainer}>
+        {cardArr.map((eachCard, index) => (
+          <div className="card" style={{ width: '18rem' }} key={index}>
+            <img
+              src={`../../../assets/projects/${eachCard.image}.png`} className="card-img-top" alt={eachCard.title}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{eachCard.title}</h5>
+              <p className="card-text">{eachCard.text}</p>
+              <a href={eachCard.link} target="_blank" className="btn btn-primary">
+                {eachCard.button}
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
